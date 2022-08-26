fc8604625d982a89b707d42f55fcef7dcf493a6f
Jean Campelo <jean93campelo@gmail.com>
2022-08-26 15:59:43 -0300

    Armazenando localmente dados do usuário e tweets

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
 const port = 5000;
 
+const dataUser = 
+{
+    "username": "Corvus",
+    "avatar": "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/63739491/1800"
+};
 
+const tweetsUser = [
+{
+    username: dataUser.username,
+    tweet: 'first post'
+},
+];
 
 server.listen(port, () => console.log(`Conectado na porta ${port}`));
