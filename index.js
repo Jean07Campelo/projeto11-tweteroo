26c824fb6a125cbeca7bcdc994fa831703725d68
Jean Campelo <jean93campelo@gmail.com>
2022-08-26 16:11:13 -0300

    Retornando mensagem de confirmação após envio de dados do usuário

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ server.use(cors());
 server.use(express.json());
 const port = 5000;
 
-const dataUser = [];
+const dataUsers = [];
 
 const tweetsUser = [
 {
-    username: dataUser.username,
+    username: dataUsers.username,
     tweet: 'first post'
 },
 ];
 
-server.post('/sign-up', (req, res) => dataUser.push(req.body));
+server.post('/sign-up', (req, res) => {
+    dataUsers.push(req.body);
+    res.send("OK");
+});
 
 server.listen(port, () => console.log(`Conectado na porta ${port}`));
