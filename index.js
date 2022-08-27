0b71c6b016cdc6a7a02fff17b657d303fadd0765
Jean Campelo <jean93campelo@gmail.com>
2022-08-27 16:36:01 -0300

    recebendo tweets do usuário e retornando mensagem OK

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const server = express();
 server.use(cors());
@@ -9,15 +9,20 @@ const port = 5000;
 const dataUsers = [];
 
 const tweetsUser = [
-{
+  {
     username: dataUsers.username,
-    tweet: 'first post'
-},
+    tweet: "first post",
+  },
 ];
 
-server.post('/sign-up', (req, res) => {
-    dataUsers.push(req.body);
-    res.send("OK");
+server.post("/sign-up", (req, res) => {
+  dataUsers.push(req.body);
+  res.send("OK");
 });
+
+server.post("/tweets" , (req, res) => {
+  tweetsUser.push(req.body);
+  res.send('OK');  
+})
 
 server.listen(port, () => console.log(`Conectado na porta ${port}`));
