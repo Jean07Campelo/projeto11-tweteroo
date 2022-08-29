b5448bb24b007ddd393373b5355ac6f439117f09
Jean Campelo <jean93campelo@gmail.com>
2022-08-29 16:11:33 -0300

    Persistindo dados do usuário

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ server.use(cors());
 server.use(express.json());
 const port = 5000;
 
-const dataUsers = [];
-
-const tweetsUser = [
+const dataUsers = [
   {
-    username: dataUsers.username,
-    tweet: "first post",
+    username: "mutano",
+    avatar:
+      "http://techjambo.com.br/wp-content/uploads/2021/11/Mutano-Jovens-Titas.png",
   },
+];
+
+const tweetsUser = [{ 
+  username: "mutano", 
+  tweet: "first tweet", 
+  avatar: dataUsers.avatar 
+},
 ];
 
 server.post("/sign-up", (req, res) => {
@@ -23,6 +29,20 @@ server.post("/sign-up", (req, res) => {
 server.post("/tweets" , (req, res) => {
   tweetsUser.push(req.body);
   res.send('OK');  
-})
+});
+
+server.get("/tweets", (req, res) => {
+
+  const tweets = 
+  {
+    username: dataUsers.username,
+    avatar: dataUsers.avatar,
+    tweet: 'teste back'
+  }
+
+  tweetsUser.push(tweets);
+  res.send(tweetsUser);
+
+});
 
 server.listen(port, () => console.log(`Conectado na porta ${port}`));
